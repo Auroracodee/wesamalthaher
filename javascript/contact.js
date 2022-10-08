07f3fb1be4a650601ecf5ece113eb12d33f08122
Auroracodee <115239967+Auroracodee@users.noreply.github.com>
2022-10-08 07:52:12 +0300

    Update contact.js

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,18 +1,7 @@
 function form_success() {
   document.getElementById("myform").submit();
   document.getElementById("submit").innerHTML = "Sent!";
-} else{
-  function goodbye(e) {
-    if (!e) e = window.event;
-    e.cancelBubble = true;
-    e.returnValue = 'Are you sure you want to leave?'; 
-    //e.stopPropagation works in Firefox.
-    if (e.stopPropagation) {
-        e.stopPropagation();
-        e.preventDefault();
-    }
-}
-window.onbeforeunload = goodbye;
+} 
 
 function isNumberKey(evt) {
     var charCode = (evt.which) ? evt.which : evt.keyCode
