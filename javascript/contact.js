f6913eb9abbda4a5d171fe21dc5a9fca5d6cc5dc
Auroracodee <115239967+Auroracodee@users.noreply.github.com>
2022-10-08 08:29:14 +0300

    Update contact.js

--- javascript/contact.js
+++ javascript/contact.js
@@ -22,3 +22,9 @@ function isNumberKey(evt) {
 
 var today = new Date().toISOString().split('T')[0];
 document.getElementsByName("setTodaysDate")[0].setAttribute('min', today);
+
+function handle_form_submission()
+{
+  alert('Submit button pressed');
+  return false; 
+}
