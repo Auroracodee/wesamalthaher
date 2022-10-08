d1bde47809e51f496be6e30fbeb0a1fad6e8aa93
Auroracodee <115239967+Auroracodee@users.noreply.github.com>
2022-10-08 08:26:32 +0300

    Add files via upload

---
+++ javascript/contact.js
@@ -0,0 +1,24 @@
+function goodbye(e) {
+    if (!e) e = window.event;
+    //e.cancelBubble is supported by IE - this will kill the bubbling process.
+    e.cancelBubble = true;
+    e.returnValue = 'You sure you want to leave?'; //This is displayed on the dialog
+
+    //e.stopPropagation works in Firefox.
+    if (e.stopPropagation) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+}
+window.onbeforeunload = goodbye;
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode != 46 && charCode != 45 && charCode > 31 &&
+        (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+var today = new Date().toISOString().split('T')[0];
+document.getElementsByName("setTodaysDate")[0].setAttribute('min', today);
